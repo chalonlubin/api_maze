894f1ad55bf89bc7fcead2b4cfaa91ba5c014fde
Chalon Lubin <chalonlubin1@gmail.com>
2022-11-16 17:38:10 -0800

    final commit

--- tvmaze.js
+++ tvmaze.js
@@ -5,9 +5,10 @@ const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
 const URL_SHOW_SEARCH = "http://api.tvmaze.com/search/shows";
-const URL_SHOW_ID = "http://api.tvmaze.com/shows/"
-const DEFAULT_IMAGE = "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300";
-const URL_EPISODE_SEARCH = "http://api.tvmaze.com/shows/[showid]/episodes"
+const URL_SHOW_ID = "http://api.tvmaze.com/shows/";
+const DEFAULT_IMAGE =
+  "https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300";
+const URL_EPISODE_SEARCH = "http://api.tvmaze.com/shows/[showid]/episodes";
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -16,19 +17,16 @@ const URL_EPISODE_SEARCH = "http://api.tvmaze.com/shows/[showid]/episodes"
  *    (if no image URL given by API, put in a default image URL)
  */
 
-
 //**  getShowsByTerm: async function that gets response based on text in input field from tvmaze API and returns the data array. */
 async function getShowsByTerm(input) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
 
-  const response = await axios.get(
-    `${URL_SHOW_SEARCH}`,
-    { params: { q: input}}
-  )
+  const response = await axios.get(`${URL_SHOW_SEARCH}`, {
+    params: { q: input },
+  });
 
-  return response.data.map(apiResp => {
-
-    const show = apiResp.show
+  return response.data.map((apiResp) => {
+    const show = apiResp.show;
 
     return {
       id: show.id,
@@ -38,8 +36,6 @@ async function getShowsByTerm(input) {
     };
   });
 }
-
-
 
 /** Given list of shows, create markup for each and to DOM */
 
@@ -55,7 +51,7 @@ function populateShows(shows) {
     // console.log(show.show.image.medium) // TEST
 
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
               src=${show.image}
@@ -70,18 +66,17 @@ function populateShows(shows) {
            </div>
          </div>
        </div>
-      `);
+      `
+    );
 
-      $showsList.append($show);
-    }
+    $showsList.append($show);
+  }
 
-      $showsList.on('click', function(evt){
-        console.log('btnclick')
-        episodeConstruct(evt);
-      }
-      )
+  $showsList.on("click", function (evt) {
+
+    episodeConstruct(evt);
+  });
 }
-
 
 /** searchForShowAndDisplay: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
@@ -101,41 +96,44 @@ $searchForm.on("submit", async function (evt) {
 
 
 
-async function episodeConstruct(evt){
+async function episodeConstruct(evt) {
   const showId = getBtnId(evt);
-  const episodes = await getEpisodesOfShow(showId)
-  console.log('from constructor',episodes)
-}
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
 
+}
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
 
 async function getEpisodesOfShow(id) {
-  const episodes = await axios.get(
-    `${URL_SHOW_ID}${id}/episodes`
-  )
-  console.log('episodes array', episodes)
-  return episodes.data.map(episode => {
+  const episodes = await axios.get(`${URL_SHOW_ID}${id}/episodes`);
 
+  return episodes.data.map((episode) => {
     return {
       id: episode.id,
       name: episode.name,
       season: episode.season,
-      number: episode.number
+      number: episode.number,
     };
   });
 }
-
 
 /** Write a clear docstring for this function... */
 
 function populateEpisodes(episodes) {
 
+
+  for (let episode of episodes) {
+    console.log({episode})
+    const $newLi = $("<li>");
+    $episodesArea.append($newLi.text(`${episode.name}, Season: ${episode.season}, Number: ${episode.number}`));
+  }
+  $episodesArea.show();
 }
 
-function getBtnId(evt){
-  const showId = $(evt.target).closest('.Show').data("show-id");
-  return showId
+function getBtnId(evt) {
+  const showId = $(evt.target).closest(".Show").data("show-id");
+  return showId;
 }
